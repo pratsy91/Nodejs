3d76f989ad4dfa23dea6201d192ba067fda3d1dc
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-04-19 15:00:40 +0530

    first response

--- first-app.js
+++ first-app.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write("<body><h1>My Nodejs server!</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(3000);
